3aebcd13a7954489f89a418e6e17ce3fdfb93332
Thant Htet Aung <maung.good@gmail.com>
2023-02-09 11:36:51 +0800

    bst insert and queue

--- binary_tree.py
+++ binary_tree.py
@@ -12,13 +12,6 @@ class BST:
         if self.root is None:
             self.root = BSTNode(data)
         else:
-            if self.root.left is None and data < self.root.data:
-                new_node = BSTNode(data)
-                self.root.left = new_node
-            elif self.root.right is None and data > self.root.data:
-                new_node = BSTNode(data)
-                self.root.right = new_node
-
             iterator = self.root
             while iterator is not None:
                 current = iterator
@@ -34,9 +27,29 @@ class BST:
             else:
                 current.right = new_node
 
-    def is_leaf(self, node):
-        if node.left is None and node.right is None:
-            return True
+    def is_leaf(self, data):
+        root = self.root
+
+        while root is not None:
+            current = root
+            if root.data == data:
+                if root.left is None and root.right is None:
+                    return True
+                else:
+                    return False
+            elif data < root.data:
+                root = root.left
+            else:
+                root = root.right
+
+        return False
+
+    def find_max(self, root):
+
+        while root is not self.is_leaf(root):
+            root = root.left
+        return root
+
 
     def level_order_traverse(self):
         if self.root is None:
@@ -49,10 +62,10 @@ class BST:
         while not q.is_empty():
             node = q.dequeue()
             result.append(node)
-            if node.get_left() is not None:
-                q.enqueue(node.get_left())
-            if node.get_right() is not None:
-                q.enqueue(node.get_right())
+            if node.left is not None:
+                q.enqueue(node.left)
+            if node.right is not None:
+                q.enqueue(node.right)
         return result
 
     def in_order_traverse(self, root, result):
@@ -61,6 +74,7 @@ class BST:
         self.in_order_traverse(root.left, result)
         result.append(root.data)
         self.in_order_traverse(root.right, result)
+        ''' The reason why I use recursive is iterative will also cost Time => O(n) and Space => O(n)'''
 
     def __str__(self):
         output = ''
@@ -96,5 +110,11 @@ if __name__ == '__main__':
     print(bst.root.right.left.right.data)
     print(bst.root.right.left.left.data)
     print(bst)
+    dummy_list = bst.level_order_traverse()
+    s = ''
+    for i in dummy_list:
+        s += str(i.data) + '\t'
+    print(s)
+    print(bst.is_leaf(17))
 
 
